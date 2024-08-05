32adb16a2be59c534ed251a7f3a95e357dc609ef
Anthony Gavidia-Vasquez <agavivasq@gmail.com>
2024-08-05 13:26:40 -0400

    basic set up

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { createClient } from 'contentful';
+import { config } from '../config';
+import './App.css'
+
+function App() {
+  const [imageCollection, setImageCollection] = useState([]); 
+
+  useEffect(() => {
+    const contentfulClient = createClient({
+      space: config.contentful.space,
+      environment: config.contentful.environment,
+      accessToken: config.contentful.accessToken
+    });
+
+    contentfulClient.getAssets()
+      .then((response) => {
+        setImageCollection(response.items); 
+      })
+      .catch((error) => {
+        console.error('Oops! Something went wrong fetching images:', error);
+      });
+  }, []);
+
+  return (
+    <div id="images">
+      {imageCollection.map((image) => (
+        <div key={image.sys.id}>
+          <img src={'https:' + image.fields.file.url} alt={image.fields.title || 'Mystery Image'} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App
