e3c413c8c2a4743e5f789be25b0585bec4340058
Anthony Gavidia-Vasquez <agavivasq@gmail.com>
2024-08-06 17:59:52 -0400

    template land section

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -3,7 +3,26 @@ import React from 'react';
 function Landing() {
   return (
     <div>
-        <div className='land'></div>
+        <div className='land'>
+          <div className='land-text'>
+            <h1>Title</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+               nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu 
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non 
+                proident, sunt in culpa qui officia deserunt mollit anim id 
+                est laborum.
+              </p>
+          </div>
+          <div className='land-cols'>
+            <div className='box1'></div>
+            <div className='box2'></div>
+            <div className='box3'></div>
+            <div className='box4'></div>
+          </div>
+        </div>
     </div>
   );
 }
