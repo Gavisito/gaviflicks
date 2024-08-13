1399f00b71bf275ed7784640e2297187c6388ee9
Anthony Gavidia-Vasquez <agavivasq@gmail.com>
2024-08-13 15:41:34 -0400

    intro vert acrousel adj respdes

--- src/components/homecontent.jsx
+++ src/components/homecontent.jsx
@@ -7,16 +7,21 @@ function HomeContent({ data }) {
 
   return (
     <div className='homecontent'>
-      <h1 className='intro-title'>Lorem Ipsum</h1>
       <div className='intro'>
-      <p className='intro-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-        exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat. Duis aute irure
-        dolor in reprehenderit in voluptate 
-        velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.</p>
+          <div className='intro-text'>
+              <h1 className='intro-title'>Lorem Ipsum</h1>
+              <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure
+                dolor in reprehenderit in voluptate 
+                velit esse cillum dolore eu fugiat nulla pariatur.
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+                mollit anim id est laborum.
+                
+                </p>
+          </div>
         <div className='hcboxes'>
           {selectimg.map((image) => (
             <div
