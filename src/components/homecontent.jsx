3f71a3e17111e6d5b02a37d3be62c4b9a96079c5
Anthony Gavidia-Vasquez <agavivasq@gmail.com>
2024-08-15 13:14:12 -0400

    made new intro text section component

--- src/components/homecontent.jsx
+++ src/components/homecontent.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import HCIntroText from './introtextsection';
 
 function HomeContent({ data }) {
   const navigate = useNavigate();
@@ -11,27 +12,7 @@ function HomeContent({ data }) {
   return (
     <div className='homecontent'>
       <div className='intro'>
-          <div className='intro-text'>
-              <h1 className='intro-title'>Lorem Ipsum</h1>
-              <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                dolor in reprehenderit in voluptate 
-                velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit anim id est laborum.
-                
-                </p>
-                <button
-            className="button-link"
-            onClick={() => navigate('/about')}
-          >
-            Learn Me
-          </button>
-          </div>
-
+        <HCIntroText/>
         <div className='hcboxes'>
           {selectimg.map((image) => (
             <div
