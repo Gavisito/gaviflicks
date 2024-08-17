bf0ef1e343f681606d7e2bb983f0f8add0fd5d46
Anthony Gavidia-Vasquez <agavivasq@gmail.com>
2024-08-17 00:45:35 -0400

    designed popup layout

--- src/components/imageStyles/imgpopup.jsx
+++ src/components/imageStyles/imgpopup.jsx
@@ -5,25 +5,27 @@ function ImgPopUP({ data, onClose }) {
   return (
     <>
       {data && (
-        <div className="popup" onClick={onClose}>
-          <h2>{data.title}</h2>
-          <div className="popup-content">
-            <img
-              className="img-popup"
-              src={`https:${data.url}`}
-              alt={data.title}
-            />
-            <div className="popup-text">
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                mollit anim id est laborum.
-              </p>
+        <div className="popup">
+            <div style={{justifyContent:'center', display:'flex'}}>
+                <button className="exit-button" onClick={onClose}>
+                    &times;
+                </button>
             </div>
-          </div>
+            <h2>{data.title}</h2>
+            <div className="popup-content">
+                <img
+                className="img-popup"
+                src={`https:${data.url}`}
+                alt={data.title}
+                />
+                <div className="popup-text">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                    exercitation ullamco laboris nisi ut aliquip.
+                </p>
+                </div>
+            </div>
         </div>
       )}
     </>
