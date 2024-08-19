f4b2776968eb8f8e23777aa58e170e4b52b3f297
Anthony Gavidia-Vasquez <agavivasq@gmail.com>
2024-08-18 22:37:18 -0400

    ladning resdes for new layout tryout

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -14,7 +14,7 @@ function Landing({data}) {
     <div>
       <div className='land'>
         <div className='land-text'>
-          <h1 style={{fontSize:'5rem', textWrap:'balance', marginBottom:'5vh'}}>The Sky and Myself</h1>
+          <h1 style={{fontSize:'3rem', textWrap:'balance', marginBottom:'10px'}}>The Sky and Myself</h1>
         </div>
         <div className='land-cols'>
           {selectimg.map((image, index) => (
